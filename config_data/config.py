287a3d55984792d194115441ebd61113873acd45
Narek Martirosyan <narekam13@icloud.com>
2023-06-12 17:14:06 +0300

    Клонирование репозитория

--- config_data/config.py
+++ config_data/config.py
@@ -1,13 +1,20 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+
+from dotenv import find_dotenv, load_dotenv
+from pydantic import BaseSettings, SecretStr, StrictStr
 
 if not find_dotenv():
     exit("Переменные окружения не загружены т.к отсутствует файл .env")
 else:
     load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+
+class SiteSettings(BaseSettings):
+    BOT_TOKEN: SecretStr = os.getenv("BOT_TOKEN")
+    RAPID_API_KEY: SecretStr = os.getenv("RAPID_API_KEY")
+    HOST_API: SecretStr = os.getenv("HOST_API")
+
+
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
     ("help", "Вывести справку")
--- loader.py
+++ loader.py
@@ -2,5 +2,6 @@ from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
+base_config = config.SiteSettings()
 storage = StateMemoryStorage()
-bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
+bot = TeleBot(token=base_config.BOT_TOKEN.get_secret_value(), state_storage=storage)
