9ce6263504035270bf96a9c9634ce3e773547720
Narek Martirosyan <narekam13@icloud.com>
2023-09-04 23:40:43 +0300

    Был исправлен вывод некоторых команд.

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,11 +1,9 @@
 from telebot.types import Message
 from database.common.models import create_db
 from loader import bot
-from loguru import logger
 
 
 @bot.message_handler(commands=["start"])
 def bot_start(message: Message):
     create_db()
-    logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
     bot.reply_to(message, f"Привет, {message.from_user.full_name}!")
